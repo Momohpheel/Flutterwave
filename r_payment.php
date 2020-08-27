959a17a5c46719211e913a3aadbb3bf80f883bf4
Momoh Philip <momohmayowa14@gmail.com>
2020-08-27 15:00:39 +0100

    minor

--- r_payment.php
+++ r_payment.php
@@ -1,23 +1,16 @@
 <?php
-require_once "vendor/autoload.php";
 
-use GuzzleHttp\Client;
+$url = "https://api.flutterwave.com/v3/payments";
+$ch = curl_init($url);
 
-$client = new Client([
-    'base_uri' => 'https://api.flutterwave.com',
-]);
-
-$response = $client->request('POST', '/v3/payments', [
-    "header" => [
-        "Authorization" => "Bearer FLWSECK_TEST-cf744a18ea5f5c0faabb5ad92eca6217-X",
-        "Content-Type" => "application/json",
-
-    ],
-    'json' => [
+curl_setopt_array($ch, array(
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => json_encode([
         'tx_ref' => 'qwerty-12345',
         'amount' => '100',
         'currency' => 'NGN',
-        'redirect_url' => 'https://rave-pay.herokuapp.com/r_callback.php',//'C:\laragon\www\Flutterwave Intergration\callback.php',
+        'redirect_url' => 'callback.php',//'C:\laragon\www\Flutterwave Intergration\callback.php',
         'payment_options' => 'card',
         'customer' => [
             'email' => 'momohmayowa14@gmail.com',
@@ -30,62 +23,28 @@ $response = $client->request('POST', '/v3/payments', [
                 'logo'=> 'https://assets.piedpiper.com/logo.png'
                 
      ]
-
-    ],
-]);
-
-$body = $response->getBody();
-$trans = json_decode($body);
-//echo $body;
-header('Location: '.$trans->data->link);
+    ]),
+    CURLOPT_HTTPHEADER => array(
+        'Content-Type: application/json',
+        'Authorization: Bearer FLWSECK_TEST-cf744a18ea5f5c0faabb5ad92eca6217-X',
+    ),
+));
 
 
-// //sk_test_f2a6d1d7f41d7d5e23c4221cf683a56b03ea3a81
-// $url = "https://api.flutterwave.com/v3/payments";
-// $ch = curl_init($url);
+$response = curl_exec($ch);
 
-// curl_setopt_array($ch, array(
-//     CURLOPT_RETURNTRANSFER => true,
-//     CURLOPT_CUSTOMREQUEST => "POST",
-//     CURLOPT_POSTFIELDS => json_encode([
-//         'tx_ref' => 'qwerty-12345',
-//         'amount' => '100',
-//         'currency' => 'NGN',
-//         'redirect_url' => 'callback.php',//'C:\laragon\www\Flutterwave Intergration\callback.php',
-//         'payment_options' => 'card',
-//         'customer' => [
-//             'email' => 'momohmayowa14@gmail.com',
-//             'phone_number' => '08168252201',
-//             'name' => 'Momoh'
-//     ],
-//      'customization' => [
-//                 'title'=> 'Pied Piper Payments',
-//                 'description'=> 'Middleout isnt free. Pay the price',
-//                 'logo'=> 'https://assets.piedpiper.com/logo.png'
-                
-//      ]
-//     ]),
-//     CURLOPT_HTTPHEADER => array(
-//         'Content-Type: application/json',
-//         'Authorization: Bearer FLWSECK_TEST-cf744a18ea5f5c0faabb5ad92eca6217-X',
-//     ),
-// ));
+$error = curl_error($ch);
 
-
-// $response = curl_exec($ch);
-
-// $error = curl_error($ch);
-
-// if ($error){
-//     die('There was an error: '. $error);
-// }
+if ($error){
+    die('There was an error: '. $error);
+}
 
 
 
-// $trans = json_decode($response, true);
-
-// header('Location :'. $trans['data']['link']);
-// //curl_close($ch);
+$trans = json_decode($response, true);
+//echo $response;
+header('Location: '. $trans['data']['link']);
+//curl_close($ch);
 
 
 ?>
